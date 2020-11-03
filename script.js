f6ffee358426300c14918c69a98da35a846ff59a
Lisa Pääjärvi <lisapaajarvi@hotmail.com>
2020-11-03 11:49:37 +0100

    added JSdoc comments

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
+window.addEventListener('load', main);
 
-window.onload = main;
-// window.addEventListener('load', main);
-
+/** Starts the program */
 function main() {
     startSlideshow ();
 }
+/** Starts the slideshow and sets interval for changing the photo */
 function startSlideshow () {
     setInterval (changePhoto, 5000);
 }
 
+/**
+ * Changes the photo that displays in the slideshow
+ */
 function changePhoto() {
     const photos = document.querySelectorAll(".container > img");
     for (let i = 0; i < photos.length; i++) {
         if (photos[i].classList.contains("show")) {
             photos[i].classList.remove("show");
-            let nextIndex = i + 1;
-            if (nextIndex >= photos.length) {
-                nextIndex = 0;
-            }
+            let nextIndex = (i + 1) % photos.length;
             photos[nextIndex].classList.add("show");
             break;
         }
